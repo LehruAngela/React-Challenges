10c8e4de6cc7d68ff5b94791915465ca3a083b03
LehruAngela <angelalehru@gmail.com>
2019-10-12 00:07:12 +0300

    Improved functionality

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,12 +1,38 @@
 import React, { Component, Fragment } from 'react';
+import TimePicker from './timePicker';
 
 class Signup extends Component {
   state = {
-    next: false,
-    back: false,
     signupOption: '',
     step1: true,
     step2: false,
+    renderEmail: false,
+    renderPhoneNumber: false,
+    name: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    dob: '',
+    address: '',
+    bestTimeToContact: ''
+  }
+
+  componentDidMount() {
+    if (this.props.location.state.signupOption === "email") {
+      this.setState({
+        renderEmail: true
+      })
+    }
+    if (this.props.location.state.signupOption === "phoneNumber") {
+      this.setState({
+        renderPhoneNumber: true
+      })
+    }
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value })
   }
 
   handleNext = () => {
@@ -23,22 +49,43 @@ class Signup extends Component {
     })
   }
 
-  componentDidMount() {
-    this.setState({
-      signupOption: this.props.location.state.signupOption
-    })
-    console.log(this.props.location.state.signupOption === "email")
-  }
-
   handleSubmit = () => {
-    this.props.history.push('/info');
+    this.props.history.push({
+      pathname: '/info',
+      state: {
+        name: this.state.name, 
+        email: this.state.email,
+        phoneNumber: this.state.phoneNumber,
+        password: this.state.password,
+        dob: this.state.dob,
+        address: this.state.address,
+        bestTimeToContact: this.state.bestTimeToContact,
+      }
+    })
   }
 
   render() {
     return (
       <Fragment>
-        <Step1 step1={this.state.step1} next={() => this.handleNext()} />
-        <Step2 step2={this.state.step2} back={() => this.handleBack()} submit={() => this.handleSubmit()} />
+        <Step1
+          step1={this.state.step1}
+          next={() => this.handleNext()}
+          renderEmail={this.state.renderEmail}
+          renderPhoneNumber={this.state.renderPhoneNumber}
+          onChange={this.handleInputChange}
+          name={this.state.name}
+          email={this.state.email}
+          phoneNumber={this.state.phoneNumber}
+          password={this.state.password}/>
+
+        <Step2
+          step2={this.state.step2}
+          back={() => this.handleBack()}
+          submit={() => this.handleSubmit()}
+          onChange={this.handleInputChange}
+          dob={this.state.dob}
+          address={this.state.address}
+          bestTimeToContact={this.state.bestTimeToContact}/>
       </Fragment>
     )
   }
@@ -52,23 +99,23 @@ const Step1 = props => {
     <div>
       <div>
         <label htmlFor="text">Name</label>
-        <input type="text" />
+        <input type="text" name="name" value={props.name} onChange={props.onChange} />
       </div>
-      {props.signupOption &&
+      {props.renderEmail &&
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" />
+          <input type="email" name="email" value={props.email} onChange={props.onChange} />
         </div>
       }
-      {props.signupOption &&
+      {props.renderPhoneNumber &&
         <div>
           <label htmlFor="text">Phone Number</label>
-          <input type="text" required />
+          <input type="text" name="phoneNumber" value={props.phoneNumber} onChange={props.onChange} required />
         </div>
       }
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" required />
+        <input type="password" name="password" value={props.password} onChange={props.onChange} required />
       </div>
       <button onClick={props.next}>NEXT</button>
     </div>
@@ -83,12 +130,16 @@ const Step2 = props => {
   return (
     <div>
       <div>
+        <label htmlFor="text">Date of Birth</label>
+        <input type="text" name="dob" value={props.dob} onChange={props.onChange} required />
+      </div>
+      <div>
         <label htmlFor="text">Street Address</label>
-        <input type="email" required />
+        <input type="text" name="address" value={props.address} onChange={props.onChange} required />
       </div>
       <div>
         <label htmlFor="text">Best Time to Contact</label>
-        {/* <TimePicker /> */}
+        <TimePicker />
       </div>
       <button onClick={props.back}>BACK</button>
       <button onClick={props.submit}>SUBMIT</button>
--- src/components/userDetails.js
+++ src/components/userDetails.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class UserDetails extends Component {
+  state = {
+    name: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    dob: ''
+  }
+  componentDidMount() {
+    console.log(this.props.location.state.password)
+    this.setState({
+      name: this.props.location.state.name,
+      email: this.props.location.state.email,
+      phoneNumber: this.props.location.state.phoneNumber,
+      password: this.props.location.state.password,
+      dob: this.props.location.state.dob,
+    })
+  }
+
   render() {
+    const { name, email, phoneNumber, password, dob, address } = this.state;
     return (
       <div>
-        {this.props.details}
+        Name: {name}
+        {email &&
+          <div>
+            Email: {email}
+          </div>
+        }
+        {phoneNumber &&
+          <div>
+            Phone Number: {phoneNumber}
+          </div>
+        }
+        Password: {password}
+        Date of Birth: {dob}
+        Street Address: {address}
+        
+
+
       </div>
     )
   }
