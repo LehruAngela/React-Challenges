70188635a5453fff4950763c604f3b2331506bd4
LehruAngela <angelalehru@gmail.com>
2019-10-24 16:08:08 +0300

    Change names and titles

--- src/components/hoc/pickOption.js
+++ src/components/hoc/pickOption.js
@@ -3,19 +3,24 @@ import React, { Component } from 'react';
 const pickOption = WrappedComponent => {
   class PickUpOption extends Component {
     state = {
-      signupOption: '',
+      signupOption: 'email',
     }
     
     handleChange = () => { 
       let value = document.getElementById('select').value
-      this.setState({ signupOption: value });
+      this.setState({ signupOption: value }, ()=>{console.log(this.state.signupOption)});
     };
+
+    onSubmit = () => {
+      
+    }
 
     render() {
       return (
         <WrappedComponent
           signupOption={this.state.signupOption}
           handleChange={this.handleChange}
+          onSubmit={this.onSubmit}
           {...this.props}
         />
       )
--- src/components/home.js
+++ src/components/home.js
@@ -4,16 +4,16 @@ import pickOption from './hoc/pickOption';
 
 class Home extends Component {
   render() {
-    const { handleChange } = this.props
+    const { handleChange, signupOption, onSubmit } = this.props
     return (
       <div>
         <h4>Choose to signup with either email or phone number</h4>
-        <div>
-          <select onChange={handleChange} id="select" className="select">
+        <form onSubmit={onSubmit}>
+          <select value={signupOption} onChange={handleChange} id="select" className="select">
             <option value="email">Email</option>
             <option value="phoneNumber">Phone Number</option>
           </select>
-        </div>
+        </form>
         <div>
           <Link to="/signup"><button type="submit">Submit</button></Link>
         </div>
